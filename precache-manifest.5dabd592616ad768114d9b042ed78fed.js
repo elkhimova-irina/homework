2a83b718ac96d48053b3998a8254deb3e876391a
elkhimova-irina <elkhimova-irina@mail.ru>
2020-05-14 11:33:37 +0300

    Updates

---
+++ precache-manifest.5dabd592616ad768114d9b042ed78fed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21348a593ed7c942f8f9fe1053765e1",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "46c4266456937436a598",
+    "url": "/homework/static/css/main.ad97cba2.chunk.css"
+  },
+  {
+    "revision": "06ddcbfd36fa01632206",
+    "url": "/homework/static/js/2.19c37aa0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework/static/js/2.19c37aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c4266456937436a598",
+    "url": "/homework/static/js/main.53e44290.chunk.js"
+  },
+  {
+    "revision": "80714701a36cd7064d67",
+    "url": "/homework/static/js/runtime-main.18ecff0a.js"
+  },
+  {
+    "revision": "23511fe6441f603c94ee4a29d1109d07",
+    "url": "/homework/static/media/euro.23511fe6.gif"
+  },
+  {
+    "revision": "2545b2c8d343707df2c7cf1097e13c64",
+    "url": "/homework/static/media/norway.2545b2c8.gif"
+  },
+  {
+    "revision": "a0505106552d9bc3007d83fbdbd91ef2",
+    "url": "/homework/static/media/shveciya.a0505106.gif"
+  },
+  {
+    "revision": "8d9d136e9cb414d541b1b6b3d614cfcd",
+    "url": "/homework/static/media/usa.8d9d136e.gif"
+  }
+]);
